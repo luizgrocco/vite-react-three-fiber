52adf96075bb2c6ebdc8dd800b914659b49007b6
Luiz Rocco <luizgustavorocco@gmail.com>
2023-02-18 17:51:19 -0300

    Lessons 9 through 16

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,34 +1,31 @@
+import { useRef, useState, useEffect, useMemo } from 'react';
 import { useFrame } from '@react-three/fiber';
-import { useEffect, useRef, useState } from 'react';
+import * as THREE from 'three';
 
-export const Box = (props) => {
+export default function Polyhedron(props) {
   const ref = useRef();
-  const [hovered, setHovered] = useState(false);
-  const [rotate, setRotate] = useState(false);
+  const [count, setCount] = useState(0);
+  const geometry = useMemo(
+    () => [new THREE.BoxGeometry(), new THREE.SphereGeometry(0.7)],
+    []
+  );
 
   useEffect(() => {
-    console.log(ref);
-  }, []);
+    console.log(ref.current.geometry.uuid);
+  });
 
   useFrame((_, delta) => {
-    if (rotate) {
-      ref.current.rotation.x += 1 * delta;
-      ref.current.rotation.y += 0.5 * delta;
-    }
+    ref.current.rotation.x += delta;
+    ref.current.rotation.y += 0.5 * delta;
   });
 
   return (
     <mesh
       {...props}
       ref={ref}
-      scale={hovered ? [1.1, 1.1, 1.1] : [1, 1, 1]}
-      onPointerDown={() => setRotate((prev) => !prev)}
-      // onPointerUp={(e) => console.log('pointer up ', e.object.name)}
-      onPointerOver={() => setHovered(true)}
-      onPointerOut={() => setHovered(false)}
-      onUpdate={(self) => console.log(self)}>
-      <boxGeometry />
-      <meshBasicMaterial color={hovered ? 0xff0000 : 0x00ff00} wireframe />
+      onPointerDown={() => setCount((count + 1) % 2)}
+      geometry={geometry[count]}>
+      <meshBasicMaterial color={'lime'} wireframe />
     </mesh>
   );
-};
+}
